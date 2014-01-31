3a766104a9cb350580a5815d1647ff919832c31d
Peter Dobson <peter@duckspot.com>
2014-01-31 10:08:05 -0800

    Rename.

---
+++ src/test/java/com/duckspot/makesite/FilenameUtilTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.duckspot.makesite;
+
+import java.io.File;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Peter.Dobson
+ */
+public class FilenameUtilTest extends TestCase {
+    
+    public FilenameUtilTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of mkdirFor method, of class FileUtil.
+     */
+    public void testMkdirFor() {
+        System.out.println("mkdirFor");
+        File f = new File("target/website/foo.html");
+        FilenameUtil.mkdirFor(f);
+        assertTrue(f.getParentFile().exists());
+    }
+
+    /**
+     * Test of getFilename method, of class FileUtil.
+     */
+    public void testGetFilename() {
+        System.out.println("getFilename");
+        String filenameExt = "index.md";
+        String expResult = "index";
+        String result = FilenameUtil.getFilename(filenameExt);
+        assertEquals(expResult, result);        
+    }
+    
+}
