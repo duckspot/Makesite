f0a9e5f52e62a934dd1fcfc27b5b45fd13823a69
Peter Dobson <peter@duckspot.com>
2014-01-31 09:38:22 -0800

    Add getLastExt() and removeLastExt() static methods.

--- src/main/java/com/duckspot/makesite/FilenameUtil.java
+++ src/main/java/com/duckspot/makesite/FilenameUtil.java
@@ -1,19 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.duckspot.makesite;
 
 import java.io.File;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- *
- * @author Peter.Dobson
- */
 public class FilenameUtil {
     
     public static void mkdirFor(File f) {
@@ -27,13 +17,44 @@ public class FilenameUtil {
     }
     
     private static final 
-            Pattern findFilenameExt = Pattern.compile("^(.*)(\\.[^.]+)$");
+            Pattern findFilename = Pattern.compile("^([^\\.]*)");
+    
     public static String getFilename(String filenameExt) {
-        Matcher m = findFilenameExt.matcher(filenameExt);
+        Matcher m = findFilename.matcher(filenameExt);
         boolean found = m.find();
         if (found && m.groupCount() >= 1)
             return m.group(1);
         else
             return filenameExt;
     }
+    
+    private static final 
+            Pattern findExt = Pattern.compile("\\.([^\\.]+)?$");
+    
+    /**
+     * Get the last extension from a filename.
+     * 
+     * @param filenameExt
+     * @return the last filename extension including the '.' before the 
+     * extension, or "" if no extension exists.
+     */
+    public static String getLastExt(String filenameExt) {
+        Matcher m = findExt.matcher(filenameExt);
+        boolean found = m.find();
+        if (found && m.groupCount() >= 1)
+            return m.group(1);
+        else
+            return "";
+    }
+    
+    public static String removeLastExt(String filenameExt) {
+        Matcher m = findExt.matcher(filenameExt);
+        boolean found = m.find();
+        if (found && m.groupCount() >= 1) {
+            int len = filenameExt.length() - m.group(1).length();
+            return filenameExt.substring(0, len-1);
+        } else {
+            return filenameExt;
+        }
+    }    
 }
