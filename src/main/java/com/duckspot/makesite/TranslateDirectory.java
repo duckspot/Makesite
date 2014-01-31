4e68a393bb40e0bd2481780748e13b3f0486f10d
Peter Dobson <peter@duckspot.com>
2014-01-31 09:40:40 -0800

    Rename.

---
+++ src/main/java/com/duckspot/makesite/TranslateDirectory.java
@@ -0,0 +1,86 @@
+package com.duckspot.makesite;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class TranslateDirectory implements ChangeListener {
+    
+    private static Translator[] translators = {};
+    
+    public static void setTranslators(Translator[] translators) {
+        TranslateDirectory.translators = translators;
+    }
+    
+    public static void translateFile(File dstDir, File srcFile) 
+            throws IOException
+    {
+        long srcModified = srcFile.lastModified();
+        for (Translator t: translators) {
+            if (t.matches(srcFile)) {
+                File dstFile = t.getDstFile(dstDir, srcFile);
+                if (dstFile.lastModified() < srcModified) {
+                    System.out.println("translate " + 
+                            srcFile + " -> " + dstFile);
+                    try {
+                        t.translate(dstFile, srcFile);
+                    } catch (IOException ex) {
+                        Logger.getLogger(App.class.getName())
+                                .log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+        }
+    }
+        
+    private final File dstDir;
+    private final File srcDir;    
+    private final boolean watch;
+    
+    public TranslateDirectory(File dstDir, File srcDir, boolean watch) {
+        this.dstDir = dstDir;
+        this.srcDir = srcDir;
+        this.watch = watch;
+    }
+    
+    public TranslateDirectory(Path dstDir, Path srcDir, boolean watch) {
+        this(dstDir.toFile(), srcDir.toFile(), watch);
+    }
+    
+    private void innerTranslate(File dstDir, File srcDir) throws IOException {
+        for (File srcFile: srcDir.listFiles()) {
+            if (srcFile.isDirectory()) {                
+                File dstSubDir = new File(dstDir, srcFile.getName());
+                File srcSubDir = srcFile;
+                innerTranslate(dstSubDir, srcSubDir);
+            } else {
+                translateFile(dstDir, srcFile);
+            }
+        }
+    }
+    
+    void translate() throws IOException {
+        Watch w = null;
+        if (watch) {
+            w = new Watch(srcDir);
+            w.addChangeListener(this);
+            w.setup();
+        }
+        innerTranslate(dstDir, srcDir);
+        if (w != null) {
+            System.out.println("watching for file changes...");
+            w.watch();
+        }
+    }
+
+    @Override
+    public void changeOccured() {
+        try {
+            innerTranslate(dstDir, srcDir);
+        } catch (IOException ex) {
+            throw new Error("unexpected IOException", ex);
+        }
+    }
+}
